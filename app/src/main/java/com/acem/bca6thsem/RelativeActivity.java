79839b8b0b6a8300a48c6615a0cc322d125b56a6
keerushar <kiran.sharma@gurzu.com>
2024-06-17 07:31:41 +0545

    Setup android components and layouts

---
+++ app/src/main/java/com/acem/bca6thsem/RelativeActivity.java
@@ -0,0 +1,15 @@
+package com.acem.bca6thsem;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RelativeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_relative);
+    }
+}
