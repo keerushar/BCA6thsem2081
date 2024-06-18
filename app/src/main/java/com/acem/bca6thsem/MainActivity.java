0810a18908381933d0dcfe96ad461f197e9b7edb
keerushar <kiran.sharma@gurzu.com>
2024-06-18 07:23:44 +0545

    Setup event handling

--- app/src/main/java/com/acem/bca6thsem/MainActivity.java
+++ app/src/main/java/com/acem/bca6thsem/MainActivity.java
@@ -2,7 +2,12 @@ package com.acem.bca6thsem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        EditText emailEt = findViewById(R.id.emailEt);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        MainActivity.this, DashboardActivity.class);
+                startActivity(i);
+            }
+        });
+
+        emailEt.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.d("Long press" , "Long Btn pressed");
+                return false;
+            }
+        });
     }
 }
