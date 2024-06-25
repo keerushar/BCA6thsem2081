223031b145829781d754a2e8ec42ef6bb9609232
keerushar <kiran.sharma@gurzu.com>
2024-06-25 08:17:33 +0545

    feat: setup calculator app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,12 +33,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-
+    implementation("com.faendir.rhino:rhino-android:1.6.0")
 }
--- app/src/main/java/com/acem/bca6thsem/MainActivity.java
+++ app/src/main/java/com/acem/bca6thsem/MainActivity.java
@@ -3,12 +3,88 @@ package com.acem.bca6thsem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultTv, solutionTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
+
+        assignId(R.id.button_c);
+        assignId(R.id.button_open_bracket);
+        assignId(R.id.button_close_bracket);
+        assignId(R.id.button_divide);
+        assignId(R.id.button_multiply);
+        assignId(R.id.button_plus);
+        assignId(R.id.button_minus);
+        assignId(R.id.button_equals);
+        assignId(R.id.button_0);
+        assignId(R.id.button_1);
+        assignId(R.id.button_2);
+        assignId(R.id.button_3);
+        assignId(R.id.button_4);
+        assignId(R.id.button_5);
+        assignId(R.id.button_6);
+        assignId(R.id.button_7);
+        assignId(R.id.button_8);
+        assignId(R.id.button_9);
+        assignId(R.id.button_ac);
+        assignId(R.id.button_dot);
+    }
+
+    void assignId(int id) {
+        MaterialButton btn = findViewById(id);
+        btn.setOnClickListener(MainActivity.this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
+
+        if (buttonText.equals("AC")) {
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+        if (buttonText.equals("=")) {
+            solutionTv.setText(resultTv.getText());
+            return;
+        } else {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if (!finalResult.equals("Err")) {
+            resultTv.setText(finalResult);
+        }
+    }
+
+    String getResult(String data) {
+        try {
+            Context context = Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable = context.initStandardObjects();
+            return context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+        } catch (Exception e) {
+            return "Err";
+        }
     }
 }
