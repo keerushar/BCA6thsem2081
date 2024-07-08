e32dd6f17b484190eb133bf9243b38e73ca3f43a
Kiran Sharma <kiran.sharma@gurzu.com>
2024-07-08 07:18:30 +0545

    chore: update wiring of fragments with widgets

--- app/src/main/java/com/acem/bca6thsem/FirstFragment.java
+++ app/src/main/java/com/acem/bca6thsem/FirstFragment.java
@@ -4,61 +4,46 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FirstFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FirstFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View v = inflater.inflate(R.layout.fragment_first, container, false);
+
+        EditText firstEt = v.findViewById(R.id.firstNumEt);
+        EditText secondEt = v.findViewById(R.id.secondNumEt);
+
+        Button addBtn = v.findViewById(R.id.addBtn);
+        TextView resultTv = v.findViewById(R.id.resultTv);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Integer firstNum = Integer.parseInt(firstEt.getText().toString());
+                    Integer secondNum = Integer.parseInt(secondEt.getText().toString());
+
+                    Integer sum = firstNum + secondNum;
+                    resultTv.setText("Result is" +sum);
+
+                } catch (Exception e) {
+                    Log.e("Exception occur", "Parse Exception");
+                }
+
+            }
+        });
+
+        return v;
     }
 }
