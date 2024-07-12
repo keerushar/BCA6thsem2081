d897fa96416818a0b38ccc16fb256657f724eaee
Kiran Sharma <kiran.sharma@gurzu.com>
2024-07-12 08:30:23 +0545

    feat: setup option menu

--- app/src/main/java/com/acem/bca6thsem/MainActivity.java
+++ app/src/main/java/com/acem/bca6thsem/MainActivity.java
@@ -1,8 +1,14 @@
 package com.acem.bca6thsem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        TextView titleTxt = findViewById(R.id.titleTxt);
+
+        switch (item.getItemId()) {
+            case R.id.item1:
+                titleTxt.setText(item.getTitle());
+                return true;
+
+            case R.id.item2:
+                titleTxt.setText(item.getTitle());
+                return true;
+
+            case R.id.item3:
+                titleTxt.setText(item.getTitle());
+                return true;
+
+            case R.id.item4:
+                titleTxt.setText(item.getTitle());
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
