bf3218c9f76096e21ca038a1c90ae4f47966097b
Kiran Sharma <kiran.sharma@gurzu.com>
2024-07-12 07:27:43 +0545

    chore: lab 6 tab layout

--- app/src/main/java/com/acem/bca6thsem/MainActivity.java
+++ app/src/main/java/com/acem/bca6thsem/MainActivity.java
@@ -1,8 +1,13 @@
 package com.acem.bca6thsem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.acem.bca6thsem.adapter.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Home");
+                    break;
+                case 1:
+                    tab.setText("Dashboard");
+                    break;
+                case 2:
+                    tab.setText("Settings");
+                    break;
+            }
+        }).attach();
     }
 }
---
+++ app/src/main/java/com/acem/bca6thsem/adapter/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.acem.bca6thsem.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import com.acem.bca6thsem.DashboardFragment;
+import com.acem.bca6thsem.HomeFragment;
+import com.acem.bca6thsem.SettingsFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return new DashboardFragment();
+            case 2:
+                return new SettingsFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
+
