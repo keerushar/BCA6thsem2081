6c2766b3567c10f813ef1496451d1c7b2b5da00d
Kiran Sharma <kiran.sharma@gurzu.com>
2024-07-30 17:53:09 +0545

    feat: setup gridview

--- app/src/main/java/com/acem/bca6thsem/GridViewActivity.java
+++ app/src/main/java/com/acem/bca6thsem/GridViewActivity.java
@@ -1,19 +1,51 @@
 package com.acem.bca6thsem;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class GridViewActivity extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_grid_view);
+
+        gridView = findViewById(R.id.gridView);
+
+        String[] items = {
+                "Pokhara",
+                "Kathmandu",
+                "Butwal",
+                "Sindhuli", "Dharan", "Pokhara",
+                "Kathmandu",
+                "Butwal",
+                "Sindhuli", "Dharan", "Pokhara",
+                "Kathmandu",
+                "Butwal",
+                "Sindhuli", "Dharan", "Pokhara",
+                "Kathmandu",
+                "Butwal",
+                "Sindhuli", "Dharan"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        gridView.setAdapter(adapter);
+
+        // Set item click listener
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), value, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
