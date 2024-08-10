29a24686c7ac2e850a1a5ead68b23da650f27658
Kiran Sharma <kiran.sharma@gurzu.com>
2024-08-10 22:11:47 +0545

    setup mapview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,5 +40,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
 }
--- app/src/main/java/com/acem/bca6thsem/MainActivity.java
+++ app/src/main/java/com/acem/bca6thsem/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng kathmandu = new LatLng(27.7172, 85.3240);
+        googleMap.addMarker(new MarkerOptions().position(kathmandu).title("Marker in Kathmandu"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kathmandu, 10));
     }
 }
